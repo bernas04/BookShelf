5dc65c74f76a8918706c34977bb35862e0621920
MartaFradique <martafradique@ua.pt>
2022-05-26 23:08:38 +0100

    top right part

--- bookshelf/src/components/TopBar/Topbar.js
+++ bookshelf/src/components/TopBar/Topbar.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Topbar.css';
+import { BsAlignBottom, BsPersonFill } from "react-icons/bs";
 
 
 
@@ -9,6 +10,22 @@ const handleLogout = () => {
 };
 
 function Topbar() {
+  const [state, setstate] = useState(false);
+  const [state2, setstate2] = useState(false);
+
+  const showDropdown=()=>{
+    setstate(true);
+  }
+  const hideDropdown=()=>{
+    setstate(false);
+  }
+  const showDropdown2=()=>{
+    setstate2(true);
+  }
+  const hideDropdown2=()=>{
+    setstate2(false);
+  }
+
   return (
     <div className="topbar">
       <div className="topbarWrapper">
@@ -16,16 +33,48 @@ function Topbar() {
           <div className="title">Bookshelf</div>
         </div>
         <div className="componentsWrapper">
-          <div className="components">Search by Category</div>
-          <div className="components">Best Sellers</div>
+          <div className="dropdown">
+            <div className="dropdown-menu" onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+              
+              <div className="components">Search by Category</div>
+              {state?(<ul className="dropdown-list" onMouseEnter={showDropdown}>
+                <ul className="items">Drama</ul>
+                <ul className="items">Romance</ul>
+                <ul className="items">Fiction</ul>
+                <ul className="items">Manga</ul>
+
+              </ul>):
+              null}
+              
+            </div>
+          </div>
+          
+          <div className="components2">Best Sellers</div>
         </div>
+
+
+        <div className="dropdown">
+            <div className="dropdown-menu" onMouseEnter={showDropdown2} onMouseLeave={hideDropdown2}>
+              
+              <div className="components"><BsPersonFill style={{position: 'absolute'}} size="35px" color="white"/></div>
+              {state2?(<ul className="dropdown-list" onMouseEnter={showDropdown2}>
+                <ul className="items">Drama</ul>
+                <ul className="items">Romance</ul>
+                <ul className="items">Fiction</ul>
+                <ul className="items">Manga</ul>
+
+              </ul>):
+              null}
+              
+            </div>
+          </div>
         
-        <div className="topRigth">
+        {/* <div className="topRigth">
           <div className="icons" onClick={() => handleLogout()}>
             Logout
-            {/* <PersonOutlineRoundedIcon /> */}
+            <PersonOutlineRoundedIcon />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
